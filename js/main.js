ecc4167a956bdc1077099311e5b4c5cfa6b992f9
nikoreserved <nikoreserved@gmail.com>
2023-02-09 14:53:49 +0200

    add: video section, delete: swiper section (#4)

--- js/main.js
+++ js/main.js
@@ -5,45 +5,6 @@ window.addEventListener("load", windowLoad);
 function windowLoad() {
   document.body.classList.add("loaded");
 
-  if (document.querySelector(".main-slider")) {
-    new Swiper(".main-slider", {
-      speed: 2000,
-      effect: "fade",
-      autoplay: {
-        delay: 5000,
-      },
-      pagination: {
-        el: ".bullets__items",
-        type: "bullets",
-        clickable: "true",
-      },
-    });
-  }
-  document.addEventListener("click", documentActions);
-  function documentActions(e) {
-    const targetElement = e.target;
-    if (targetElement.closest(".nav-popular__item")) {
-      const tabNavItem = targetElement.closest(".nav-popular__item");
-      if (!tabNavItem.classList.contains("active")) {
-        const activeTabNavItem = document.querySelector(
-          ".nav-popular__item.active"
-        );
-        activeTabNavItem.classList.remove("active");
-        tabNavItem.classList.add("active");
-
-        const tabItems = document.querySelectorAll(".popular__tab");
-        const activeTabItem = document.querySelector(".popular__tab.active");
-
-        activeTabItem.classList.remove("active");
-        tabItems[getIndex(tabNavItem)].classList.add("active");
-      }
-    }
-  }
-
-  function getIndex(el) {
-    return Array.from(el.parentNode.children).indexOf(el);
-  }
-
   const items = document.querySelectorAll("[data-item]");
   const options = {
     threshold: 0.2,
