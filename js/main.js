a6c9b9abffd32ff8fed40a0028032549ac920938
nikopapchenko <nikopapchenko@gmail.com>
2023-03-06 13:11:37 +0200

    add SmoothScroll js (#8)

--- js/main.js
+++ js/main.js
@@ -129,3 +129,17 @@ const lightbox = GLightbox({
   loop: true,
   autoplayVideos: true
 });
+
+// scroll
+SmoothScroll({
+    animationTime    : 800,
+    stepSize         : 75,
+    accelerationDelta : 30,  
+    accelerationMax   : 2,   
+    keyboardSupport   : true,  
+    arrowScroll       : 50,
+    pulseAlgorithm   : true,
+    pulseScale       : 4,
+    pulseNormalize   : 1,
+    touchpadSupport   : true,
+})
