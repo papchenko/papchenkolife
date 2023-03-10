e39716aa87f40d57ae158a85bd2aa9eb45e680ee
nikopapchenko <nikopapchenko@gmail.com>
2023-03-10 11:21:15 +0200

    add scroll on main page (#9)

--- js/main.js
+++ js/main.js
@@ -145,15 +145,44 @@ SmoothScroll({
 })
 
 // goto
-const smoothLinks = document.querySelectorAll('a[href^="#go"]');
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
+const ssMoveTo = function(){
 
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
+  const easeFunctions = {
+      easeInQuad: function (t, b, c, d) {
+          t /= d;
+          return c * t * t + b;
+      },
+      easeOutQuad: function (t, b, c, d) {
+          t /= d;
+          return -c * t* (t - 2) + b;
+      },
+      easeInOutQuad: function (t, b, c, d) {
+          t /= d/2;
+          if (t < 1) return c/2*t*t + b;
+          t--;
+          return -c/2 * (t*(t-2) - 1) + b;
+      },
+      easeInOutCubic: function (t, b, c, d) {
+          t /= d/2;
+          if (t < 1) return c/2*t*t*t + b;
+          t -= 2;
+          return c/2*(t*t*t + 2) + b;
+      }
+  }
+
+  const triggers = document.querySelectorAll('.smoothscroll');
+  
+  const moveTo = new MoveTo({
+      tolerance: 0,
+      duration: 1800,
+      easing: 'easeInOutCubic',
+      container: window
+  }, easeFunctions);
+
+  triggers.forEach(function(trigger) {
+      moveTo.registerTrigger(trigger);
+  });
+
 };
+
+ssMoveTo();
