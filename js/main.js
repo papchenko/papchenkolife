55735031bd6cceb65fb8d831d44a675e35ed5e3a
nikopapchenko <nikopapchenko@gmail.com>
2023-03-10 09:42:31 +0200

    add scroll on main page (#9)

--- js/main.js
+++ js/main.js
@@ -145,35 +145,15 @@ SmoothScroll({
 })
 
 // goto
-// const smoothLinks = document.querySelectorAll('a[href^="#go"]');
-// for (let smoothLink of smoothLinks) {
-//     smoothLink.addEventListener('click', function (e) {
-//         e.preventDefault();
-//         const id = smoothLink.getAttribute('href');
+const smoothLinks = document.querySelectorAll('a[href^="#go"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
 
-//         document.querySelector(id).scrollIntoView({
-//             behavior: 'smooth',
-//             block: 'start'
-//         });
-//     });
-// };
-
-const anchors = [].slice.call(document.querySelectorAll('a[href^="#go"]')),
-      animationTime = 800,
-      framesCount = 80;
-
-anchors.forEach(function(item) {
-  item.addEventListener('click', function(e) {
-    e.preventDefault();
-    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-    let scroller = setInterval(function() {
-      let scrollBy = coordY / framesCount;
-      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        window.scrollBy(0, scrollBy);
-      } else {
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-    }, animationTime / framesCount);
-  });
-});
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
